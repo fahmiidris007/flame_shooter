0ae920f920360ce84dc8563ff0f404c0ca1b9779
Fahmi Idris <fahmi.idris@pactindo.com>
2024-09-08 21:38:27 +0700

    add input detector

--- lib/components/ship.dart
+++ lib/components/ship.dart
@@ -1,23 +1,29 @@
 import 'dart:async';
 import 'dart:math';
-
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/flame.dart';
 
-class Ship extends SpriteComponent {
+class Ship extends SpriteComponent with TapCallbacks {
   @override
   FutureOr<void> onLoad() async {
     sprite = Sprite(await Flame.images.load('ships/spaceShips_001.png'));
-    position = Vector2(250, 250);
+    position = Vector2(100, 100);
     // size = Vector2(100, 100);
     scale = Vector2(0.5, 0.5);
     angle = pi / 2;
     anchor = Anchor.center;
   }
 
-  @override
-  void update(double dt) {
-    super.update(dt);
-    position.add(Vector2(1, 0));
-  }
+  // @override
+  // void onTapDown(TapDownEvent event) {
+  //   super.onTapDown(event);
+  //   position += Vector2(10, 0);
+  // }
+
+  // @override
+  // void update(double dt) {
+  //   super.update(dt);
+  // position.add(Vector2(1, 0));
+  // }
 }
--- lib/pages/flame_shooter_page.dart
+++ lib/pages/flame_shooter_page.dart
@@ -1,14 +1,31 @@
 import 'dart:async';
+import 'dart:developer';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_shooter/components/ship.dart';
 
-class FlameShooterPage extends FlameGame {
+class FlameShooterPage extends FlameGame with TapCallbacks, PanDetector {
+  Ship ship = Ship();
+
   @override
   FutureOr<void> onLoad() async {
     // add(SpriteComponent(
     //   sprite: await loadSprite('ships/spaceShips_001.png'),
     // ));
-    Ship ship = Ship();
+
     add(ship);
   }
+
+  @override
+  void onTapDown(TapDownEvent event) {
+    super.onTapDown(event);
+    log('TapdownEvent: ${event.localPosition}');
+    ship.position.add(Vector2(10, 0));
+  }
+
+  // @override
+  // void onPanUpdate(DragUpdateInfo info) {
+  //   super.onPanUpdate(info);
+  //   ship.position = info.eventPosition.global;
+  // }
 }
