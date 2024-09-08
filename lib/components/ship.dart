3b172547924c0dc43a41cd31c97766dc617c9a67
Fahmi Idris <fahmi.idris@pactindo.com>
2024-09-08 22:22:56 +0700

    add movement component

--- lib/components/ship.dart
+++ lib/components/ship.dart
@@ -5,14 +5,27 @@ import 'package:flame/events.dart';
 import 'package:flame/flame.dart';
 
 class Ship extends SpriteComponent with TapCallbacks {
+  late Vector2 destination;
+  late Vector2 d; // distance
+  double v = 2.0; // velocity
+
+  // dijalankan petama kali
   @override
   FutureOr<void> onLoad() async {
     sprite = Sprite(await Flame.images.load('ships/spaceShips_001.png'));
-    position = Vector2(100, 100);
+    // position = Vector2(100, 100);
     // size = Vector2(100, 100);
     scale = Vector2(0.5, 0.5);
     angle = pi / 2;
     anchor = Anchor.center;
+  }
+
+  // dijalankan setelah seluruh komponen di load (onLoad)
+  @override
+  void onMount() {
+    super.onMount();
+    d = Vector2(0, 0);
+    destination = position;
   }
 
   // @override
@@ -21,9 +34,24 @@ class Ship extends SpriteComponent with TapCallbacks {
   //   position += Vector2(10, 0);
   // }
 
-  // @override
-  // void update(double dt) {
-  //   super.update(dt);
-  // position.add(Vector2(1, 0));
-  // }
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if ((destination - position).length < v) {
+      position = destination;
+      d = Vector2(0, 0);
+    }
+    position.add(d * v);
+  }
+
+  void setDestination(DragUpdateInfo info) {
+    destination = info.eventPosition.global;
+    lookAt(destination);
+    /* arah default lookAt arah assets keatas, menyesuaikan arah awal asset 
+      (disini karena arah awal ke bawah jadi harus di keatasin / diputar 360 degree = phi) */
+    angle += pi;
+    // distance / jarak total dari posisi saat ini ke posisi tujuan
+    d = destination - position;
+    d = d.normalized(); // besar satuan (diskrit) dari distance d
+  }
 }
--- lib/pages/flame_shooter_page.dart
+++ lib/pages/flame_shooter_page.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame_shooter/components/ship.dart';
@@ -16,16 +15,17 @@ class FlameShooterPage extends FlameGame with TapCallbacks, PanDetector {
     add(ship);
   }
 
-  @override
-  void onTapDown(TapDownEvent event) {
-    super.onTapDown(event);
-    log('TapdownEvent: ${event.localPosition}');
-    ship.position.add(Vector2(10, 0));
-  }
-
   // @override
-  // void onPanUpdate(DragUpdateInfo info) {
-  //   super.onPanUpdate(info);
-  //   ship.position = info.eventPosition.global;
+  // void onTapDown(TapDownEvent event) {
+  //   super.onTapDown(event);
+  //   log('TapdownEvent: ${event.localPosition}');
+  //   ship.position.add(Vector2(10, 0));
   // }
+
+  @override
+  void onPanUpdate(DragUpdateInfo info) {
+    super.onPanUpdate(info);
+    // ship.position = info.eventPosition.global;
+    ship.setDestination(info);
+  }
 }
